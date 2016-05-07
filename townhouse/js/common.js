22a719e47f5b5bb9150671d3ebcf32b923ea408b
AndrewKudrenko <a.v.kudrenko@gmail.com>
2016-05-07 12:14:22 +0300

    nv

--- townhouse/js/common.js
+++ townhouse/js/common.js
@@ -7,4 +7,12 @@ $(document).ready(function(){
 	$('.butn.light').addClass('wow fadeInLeft');
 	$('.butn.dark').addClass('wow fadeInRight');
 
+
+	$('a[href^="#"]').click(function(){
+		var target = $(this).attr('href');
+		$('html, body').animate({scrollTop: $(target).offset().top}, 800);
+	return false;
+	});
+
 })
+
